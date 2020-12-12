05e27a050001e3db84908d7f079925ad3e0f271c
liaugust <august.ivan228@gmail.com>
2020-12-12 23:46:32 +0200

    Realize basic post adding functionality

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <div className="row">
         <div className="col">
           <h2>Синхронные Посты</h2>
-          <Posts posts={ []} />
+          <Posts />
         </div>
         <div className="col">
           <h2>Асинхронные Посты</h2>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { compose, createStore } from 'redux';
+import { Provider } from 'react-redux'
 
+import {rootReducer} from './redux/reducers/rootReducer'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer, compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+
 render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
