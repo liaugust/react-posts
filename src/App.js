cebe99d13536faa1109618f8f1f5d98eb8282a7b
liaugust <august.ivan228@gmail.com>
2020-12-12 18:49:09 +0200

    Created initial UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import FetchedPosts from "./components/FetchedPosts"
+import PostForm from "./components/PostForm"
+import Posts from "./components/Posts"
+
 const App = () => {
   return (
-    <div className="container"></div>
+    <div className="container pt-3">
+      <div className="row">
+        <div className="col">
+          <PostForm />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <h2>Синхронные Посты</h2>
+          <Posts posts={ []} />
+        </div>
+        <div className="col">
+          <h2>Асинхронные Посты</h2>
+          <FetchedPosts posts={[]} />
+        </div>
+      </div>
+    </div>
   )
 }
 export default App;
